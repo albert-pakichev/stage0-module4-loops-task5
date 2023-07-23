d9a92f121bf3b9649307a6e9a9f3692c97561d98
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-23 15:32:30 +0500

    Update Square.java

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -2,6 +2,9 @@ package school.mjc.stage0.loops.task5;
 
 public class Square {
     public void printSquareFrom8s(int sideLength){
-
+ for ( int i = 0; i < sideLength; ++i )
+            for ( int j = 0; j <= sideLength; ++j )
+                System.out.print( ( j == sideLength ) ? '\n' : ( i == 0 || i == sideLength - 1 || j == 0 || j == sideLength - 1 ) ? '*' : ' ');
+        System.out.println();
     }
 }
