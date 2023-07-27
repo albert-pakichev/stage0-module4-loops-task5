9c2e53b6d649a841930c6cd59c51a4485860998b
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-27 12:18:05 +0500

    Update Hourglass.java

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        String eight = "8";                 
+        String dot = " ";                
+        for (var i = 0; i < height; i++) {
+            for (var j = 0; j < height; j++) {
+                boolean eights = (j >= i && j <= height - 1 - i
+                        || j <= i && j >= height - 1 - i);
+                boolean dots = (i > j);
+                if(eights){
+                    System.out.print(eight);
+                } else if (dots) {
+                    System.out.print(dot);
+                }
+            }
+            System.out.println();
+        }
     }
 }
