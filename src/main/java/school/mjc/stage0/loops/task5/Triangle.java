c558c4f8374c4f9bc6d806020b469827dd5e723f
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-07-27 11:52:40 +0500

    Update Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
-
+    for(int i=0;i<cathetusLength;i++) {
+            for (int j=i;j>0;j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
